8e0072590010972151a0f9996794f2353b114cd1
troyfischer <heat444@ufl.edu>
2019-01-30 15:19:23 -0500

    code refactoring

--- Reverse_Image_Search/icobench.py
+++ Reverse_Image_Search/icobench.py
@@ -6,136 +6,184 @@ import shutil
 import time
 from urllib.request import urlopen, Request
 
-class Icobench:
-    def __init__(self):
-        pass
-    def getAllIcos(self):
-        base_url = 'https://icobench.com'
-        os.chdir('/home/troy/Documents/Senior-Design/Reverse_Image_Search')
-        df = pd.read_csv('whitepapers_original.csv')
-        # create a new directory for the data 
-        path_to_data = '/home/troy/Documents/Senior-Design/Reverse_Image_Search/data'
-        if not os.path.isdir(path_to_data):
-            os.mkdir(path_to_data)
-        # cd to new directory
-        os.chdir(path_to_data)
-        last_project = ''
-        with open('last_ico.txt', 'r') as f:
+# DEFINE EXTERNAL METHODS
+def createDirectoryAndChangeToIt(dir_name):
+    if not os.path.isdir(dir_name):
+        os.mkdir(dir_name)
+    # change to new directory
+    os.chdir(dir_name)
+
+def projectFailed():
+    team_df = pd.DataFrame(columns=cols)
+    team_df.to_csv(project_name + '.csv', index=False)
+    os.chdir('..')
+
+def getLastProject():
+    file_name = 'last_ico.txt'
+    last_project = ''
+    try:
+        with open(file_name, 'r') as f:
             for line in f:
                 last_project = line.strip()
+    except:
+        # file doesn't exist yet
+        pass
+    return last_project
 
-        # begin scraping ICOs from the last ICO completed 
-        at_current = False
+def atCurrentProject(project_name, last_project):
+    if project_name == last_project or last_project == '':
+        return True
+    return False
+
+def getPageContents(url):
+    req = Request(url, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'})
+    try:
+        html = urlopen(req).read()
+        soup = bs(html, 'lxml')
+        return soup
+    except:
+        # icobench does not have a page for this project
+        team_df = pd.DataFrame(columns=cols)
+        team_df.to_csv(project_name + '.csv', index=False)
+        os.chdir('..')
+        return False
+
+def getImage(member, save_name, base_url):
+    image_target = member.find('div', {'class':'image_background'})
+    image_target = str(image_target['style'])
+    '''
+    image target is of the form background-image:url('/images/icos/team/image.jpg')
+    need to clean it and extract just the url
+    '''
+    image_url = (image_target.replace('background-image:url(\'', '')).replace('\');', '')
+    # Also check if url points to an actual image
+    check_url = image_url.split('/')[4]
+    if check_url != 'no-image.jpg':
+        # need to attach image_url to base url for full image url
+        full_image_url = base_url + image_url
+        # download and save the image
+        r = requests.get(full_image_url, stream=True)
+        if r.status_code == 200:
+            image_save_name = save_name + '_image.jpg'
+            with open(image_save_name, 'wb') as f:
+                r.raw.decode_content = True
+                shutil.copyfileobj(r.raw, f)
+        else:
+            image_save_name = 'N/A'
+    else:
+        image_save_name = 'N/A'
+    return image_save_name
+
+def getSocialMedia(member, save_name):
+    social_medias = {} # store them in a dictionary ['Social media name'] = 'url'
+    social_div = member.find('div', {'class':'socials'})
+    if social_div:
+        socials = social_div.find_all('a')
+        for social in socials:
+            social_media = social.text.strip()
+            url = social['href']
+            social_medias[social_media] = url
+
+        '''
+        Creating a seperate file to store social media profiles.
+        They will take the form: name|url
+        '''
+        social_save_name = save_name + '_social_media.txt'
+        with open(social_save_name, 'w') as f:
+            if social_medias:
+                for social_name, url in social_medias.items():
+                    f.write(social_name + '|' + url + '\n')
+            else:
+                f.write('N/A')
+    else:
+        social_save_name = 'N/A'
+    return social_save_name
+
+
+class Icobench:
+    def __init__(self):
+        self.base_url = 'https://icobench.com'
+
+    def getAllIcos(self):
+        # retrieve all ICOs from the CSV file Professor Li provided
+        os.chdir('/home/troy/Documents/Senior-Design/Reverse_Image_Search')
+        df = pd.read_csv('whitepapers_original.csv')
+
+        # create a new directory for the data 
+        path_to_data = '/home/troy/Documents/Senior-Design/Reverse_Image_Search/data'
+        createDirectoryAndChangeToIt(path_to_data)
+
+        ''' if script fails or needs to be restarted last_ico.txt will be 
+         a reference point to not waste time rescraping already scraped projects '''
+        last_project = getLastProject()
+
         for index, row in df.iterrows():
             project_name = row['ICO_Name'].replace(' ', '_')
-            if project_name == last_project or last_project == '':
-                at_current = True
-            if not at_current:
+            if not atCurrentProject(project_name, last_project):
                 continue
+            # script will continue at the ICO before the last failed ICO
             project_url = row['ico_address']
-            print(project_name)
+            print(f'Currently gathering data for: {project_name}') # project currently being scraped
 
-            # create a new directory for this project
-            if not os.path.isdir(project_name):
-                os.mkdir(project_name)
-            # cd to new directory
-            os.chdir(project_name)
+            # create a new directory within 'data' for this project
+            createDirectoryAndChangeToIt(project_name)
 
-            # the home url of the ico
-            req = Request(project_url, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'})
-            try:
-                html = urlopen(req).read()
-            except:
-                # icobench does not have a page for this project
-                team_df = pd.DataFrame(columns=cols)
-                team_df.to_csv(project_name + '.csv', index=False)
-                os.chdir('..')
+            # request home url of the ico
+            soup = getPageContents(project_url)
+            if not soup:
+                # page doesn't exist
+                projectFailed()
                 continue
-            soup = bs(html, 'lxml')
+
+            # look for team tab
             target = soup.find('a', {'class':'team'})
             if not target:
-                print('no target acquired')
                 # project has no team -- save empty dataframe and move on to next
-                team_df = pd.DataFrame(columns=cols)
-                team_df.to_csv(project_name + '.csv', index=False)
-                os.chdir('..')
+                projectFailed()
                 continue
-            # project has a team
+
+            # webpage has a team tab
             href = target['href']
 
-            # the team url of the ico
-            team_url = base_url + href
-            req = Request(team_url, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'})
-            html = urlopen(req).read()
-            soup = bs(html, 'lxml')
+            # request team url of the ico
+            team_url = self.base_url + href
+            soup = getPageContents(team_url)
+            if not soup:
+                # page doesn't exist
+                projectFailed()
+                continue
+
+            # find all team members
             base_div = soup.find('div', {'id':'team'})
             members = base_div.find_all('div', {'class':'col_3'})
+            if not members:
+                # project has team tab but no team members
+                projectFailed()
+                continue
             '''
             For every member of the team we want to get their name, image and social media links
             '''
-            # dataframe to save them to 
+            # create dataframe to save them to 
             cols = ['Name', 'Image File', 'Social Media File']
             team_df = pd.DataFrame(columns=cols)
             for member in members:
-                # intialize variables
-                image_save_name = ''
-                social_save_name = ''
-                # name
+                # find the name of this member
                 name = member.find('h3', {'class': 'notranslate'}).text
-                # correcting for strange error
+
+                # correcting for errors in the name
                 if 'https://' in name:
                     name = 'NO_NAME'
                 if '/' in name:
                     name = name.replace('/', '-')
 
-                save_name = name.replace(' ', '_')
-                # image
-                image_target = member.find('div', {'class':'image_background'})
-                image_target = str(image_target['style'])
-                '''
-                image target is of the form background-image:url('/images/icos/team/image.jpg')
-                need to clean it and extract just the url
-                '''
-                image_url = (image_target.replace('background-image:url(\'', '')).replace('\');', '')
-                # Also check if url points to an actual image
-                check_url = image_url.split('/')[4]
-                if check_url != 'no-image.jpg':
-                    # need to attach image_url to base url for full image url
-                    full_image_url = base_url + image_url
-                    # download and save the image
-                    r = requests.get(full_image_url, stream=True)
-                    if r.status_code == 200:
-                        image_save_name = save_name + '_image.jpg'
-                        with open(image_save_name, 'wb') as f:
-                            r.raw.decode_content = True
-                            shutil.copyfileobj(r.raw, f)
-                else:
-                    image_save_name = 'N/A'
-                # social media
-                social_medias = {} # store them in a dictionary ['Social media name'] = 'url'
-                social_div = member.find('div', {'class':'socials'})
-                if social_div:
-                    socials = social_div.find_all('a')
-                    for social in socials:
-                        social_media = social.text.strip()
-                        url = social['href']
-                        social_medias[social_media] = url
+                save_name = name.replace(' ', '_') # replace spaces with underscores
 
-                    '''
-                    creating a seperate file to store social media profiles
-                    they will take the form: name|url
-                    '''
-                    social_save_name = save_name + '_social_media.txt'
-                    with open(social_save_name, 'w') as f:
-                        if social_medias:
-                            for social_name, url in social_medias.items():
-                                f.write(social_name + '|' + url + '\n')
-                        else:
-                            f.write('N/A')
-                else:
-                    social_save_name = 'N/A'
+                # IMAGE
+                image_save_name = getImage(member, save_name, self.base_url)
+                # SOCIAL MEDIA
+                social_save_name = getSocialMedia(member, save_name)
 
-                # add the team member to the team dataframe
+                # update dataframe
                 info = [name, image_save_name, social_save_name]
                 temp_df = pd.DataFrame([info], columns=cols)
                 team_df = team_df.append(temp_df, ignore_index=True)
@@ -143,122 +191,85 @@ class Icobench:
 
             # save the team dataframe
             team_df.to_csv(project_name + '.csv', index=False)
-            # move back a directory
+            # move back to 'data' directory
             os.chdir('..')
-            # record who was the last team
+            # record the last team completed
             with open('last_ico.txt', 'w') as f:
                 f.write(project_name)
 
+    ''' This method allows for the data on an individual ICO project to be downloaded '''
     def getIco(self, name, icobench_url):
-        base_url = 'https://icobench.com'
         os.chdir('/home/troy/Documents/Senior-Design/Reverse_Image_Search')
-        df = pd.read_csv('whitepapers_original.csv')
-        os.chdir('/home/troy/Documents/Senior-Design/Reverse_Image_Search/data')
-        project_name = name.replace(' ', '_')
-        project_url = icobench_url
-        print(project_name)
 
-        # create a new directory for this project
-        if not os.path.isdir(project_name):
-            os.mkdir(project_name)
-        # cd to new directory
-        os.chdir(project_name)
-        # the home url of the ico
-        req = Request(project_url, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'})
-        try:
-            html = urlopen(req).read()
-        except:
-            print('Icobench url does not lead to a valid team page... Ending Attempt.')
-            return
-        soup = bs(html, 'lxml')
+        # create a new directory for the data 
+        path_to_data = '/home/troy/Documents/Senior-Design/Reverse_Image_Search/data'
+        createDirectoryAndChangeToIt(path_to_data)
+
+        project_name = name
+        project_url = icobench_url
+        print(f'Currently gathering data for: {project_name}') # project currently being scraped
+
+        # create a new directory within 'data' for this project
+        createDirectoryAndChangeToIt(project_name)
+
+        # request home url of the ico
+        soup = getPageContents(project_url)
+        if not soup:
+            # page doesn't exist
+            projectFailed()
+
+        # look for team tab
         target = soup.find('a', {'class':'team'})
         if not target:
-            print('Project has no team... Ending Attempt.')
             # project has no team -- save empty dataframe and move on to next
-            team_df = pd.DataFrame(columns=cols)
-            team_df.to_csv(project_name + '.csv', index=False)
-            return
-        # project has a team
+            projectFailed()
+
+        # webpage has a team tab
         href = target['href']
 
-        # the team url of the ico
-        team_url = base_url + href
-        req = Request(team_url, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'})
-        html = urlopen(req).read()
-        soup = bs(html, 'lxml')
+        # request team url of the ico
+        team_url = self.base_url + href
+        soup = getPageContents(team_url)
+        if not soup:
+            # page doesn't exist
+            projectFailed()
+
+        # find all team members
         base_div = soup.find('div', {'id':'team'})
         members = base_div.find_all('div', {'class':'col_3'})
+        if not members:
+            # project has team tab but no team members
+            projectFailed()
         '''
         For every member of the team we want to get their name, image and social media links
         '''
-        # dataframe to save them to 
+        # create dataframe to save them to 
         cols = ['Name', 'Image File', 'Social Media File']
         team_df = pd.DataFrame(columns=cols)
         for member in members:
-            # intialize variables
-            image_save_name = ''
-            social_save_name = ''
-            # name
+            # find the name of this member
             name = member.find('h3', {'class': 'notranslate'}).text
-            # correcting for strange error
+
+            # correcting for errors in the name
             if 'https://' in name:
                 name = 'NO_NAME'
             if '/' in name:
                 name = name.replace('/', '-')
 
-            save_name = name.replace(' ', '_')
-            # image
-            image_target = member.find('div', {'class':'image_background'})
-            image_target = str(image_target['style'])
-            '''
-            image target is of the form background-image:url('/images/icos/team/image.jpg')
-            need to clean it and extract just the url
-            '''
-            image_url = (image_target.replace('background-image:url(\'', '')).replace('\');', '')
-            # Also check if url points to an actual image
-            check_url = image_url.split('/')[4]
-            if check_url != 'no-image.jpg':
-                # need to attach image_url to base url for full image url
-                full_image_url = base_url + image_url
-                # download and save the image
-                r = requests.get(full_image_url, stream=True)
-                if r.status_code == 200:
-                    image_save_name = save_name + '_image.jpg'
-                    with open(image_save_name, 'wb') as f:
-                        r.raw.decode_content = True
-                        shutil.copyfileobj(r.raw, f)
-            else:
-                image_save_name = 'N/A'
-            # social media
-            social_medias = {} # store them in a dictionary ['Social media name'] = 'url'
-            social_div = member.find('div', {'class':'socials'})
-            if social_div:
-                socials = social_div.find_all('a')
-                for social in socials:
-                    social_media = social.text.strip()
-                    url = social['href']
-                    social_medias[social_media] = url
+            save_name = name.replace(' ', '_') # replace spaces with underscores
 
-                '''
-                creating a seperate file to store social media profiles
-                they will take the form: name|url
-                '''
-                social_save_name = save_name + '_social_media.txt'
-                with open(social_save_name, 'w') as f:
-                    if social_medias:
-                        for social_name, url in social_medias.items():
-                            f.write(social_name + '|' + url + '\n')
-                    else:
-                        f.write('N/A')
-            else:
-                social_save_name = 'N/A'
+            # IMAGE
+            image_save_name = getImage(member, save_name, self.base_url)
+            # SOCIAL MEDIA
+            social_save_name = getSocialMedia(member, save_name)
 
-            # add the team member to the team dataframe
+            # update dataframe
             info = [name, image_save_name, social_save_name]
             temp_df = pd.DataFrame([info], columns=cols)
             team_df = team_df.append(temp_df, ignore_index=True)
-            # end for loop
 
         # save the team dataframe
         team_df.to_csv(project_name + '.csv', index=False)
+        # move back to 'data' directory
+        os.chdir('..')
 
--- Reverse_Image_Search/reverseimgsearch.py
+++ Reverse_Image_Search/reverseimgsearch.py
@@ -11,6 +11,41 @@ from urllib.request import urlopen, Request
 
 icos = pd.read_csv('whitepapers_original.csv')
 
+def get_df(root_dir, d):
+    target = root_dir + d
+    if os.path.isdir(target):
+        os.chdir(target)
+        print(d)
+        if os.path.exists(d + '.csv'):
+            df = pd.read_csv(d + '.csv')
+            return df
+    temp_df = pd.DataFrame()
+    return temp_df
+
+def useSelenium(geckodrv_path, url):
+    options = Options()
+    options.headless = True
+    driver = webdriver.Firefox(options=options, executable_path=geckodrv_path)
+    driver.get(url)
+    target = driver.find_element_by_id('cnt')
+    html = target.get_attribute('innerHTML')
+    driver.quit()
+    return html
+
+def uploadToGyazo(gyazo_client, img_file):
+    if img_file != 'N\A':
+        img_obj = gyazo_client.upload(img_file)
+        url = img_obj.url
+        return url
+    return False
+
+def reverseImageSearch(geckodrv_path, gyazo_url):
+    google_path = 'https://images.google.com/searchbyimage?image_url='
+    full_path = google_path + gyazo_url
+    html = useSelenium(geckodrv_path, full_path)
+    soup = bs(html, 'lxml')
+    targets = soup.find_all('div', {'class':'g'})
+
 #TODO
 '''
 loop through every ico
@@ -22,54 +57,28 @@ discuss what would constitute a red flag on a reverse image search
 class Google():
     def __init__(self):
         self.google_path = 'images.google.com/searchbyimage?image_url='
-        self.gyazo = GyazoObj()
+        self.gyazo_client = GyazoObj()
         self.root_dir = '/home/troy/Documents/Senior-Design/Reverse_Image_Search/data/'
         self.geckodrv = '/home/troy/Documents/Senior-Design/Reverse_Image_Search/geckodriver'
-
-    def get_df(self, d):
-        target = self.root_dir + d
-        if os.path.isdir(target):
-            os.chdir(target)
-            print(d)
-            if os.path.exists(d + '.csv'):
-                df = pd.read_csv(d + '.csv')
-                return df
-            else:
-                temp_df = pd.DataFrame()
-
-    def useSelenium(self, path):
-        options = Options()
-        options.headless = True
-        driver = webdriver.Firefox(options=options, executable_path=self.geckodrv)
-        driver.get(path)
-        target = driver.find_element_by_id('cnt')
-        html = target.get_attribute('innerHTML')
-        driver.quit()
-        return html
-
-    def reverseImageSearch(self, gyazo_url):
-        google_path = 'https://images.google.com/searchbyimage?image_url='
-        full_path = google_path + gyazo_url
-        html = self.useSelenium(full_path)
-        soup = bs(html, 'lxml')
-        targets = soup.find_all('div', {'class':'g'})
 
     def verifyAll(self):
         dirs = sorted(os.listdir(self.root_dir), key=lambda x: str.lower(x))
         for d in dirs:
-            df = self.get_df(d)
+            # TODO
+            # add support for when the directory exists but no csv exists
+            df = get_df(d)
             if df.empty:
                 print('RED FLAG')
             else:
                 for index, row in df.iterrows():
+                    name = row['Name']
                     img_file = row['Image File']
-                    if img_file != 'N\A':
-                        img_obj = self.gyazo.upload(img_file)
-                        url = img_obj.url
-                        self.reverseImageSearch(url)
+                    url = uploadToGyazo(self.gyazo_client, img_file)
+                    if url:
+                        self.reverseImageSearch(self.geckodrv, url)
                         break
                     else:
-                        print('MIGHT BE RED FLAG')
+                        print(f'NO IMAGE FILE FOR {name}... MIGHT BE RED FLAG')
                 break
                 os.chdir('..')
 
